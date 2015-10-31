0acffb3f0387bb6b261d1ffe264d5a7bfba93a0d
Mario Naranjo <mario15695@hotmail.com>
2015-10-30 18:53:16 -0600

    Centro de Ayuda - Modal

--- modules/app.js
+++ modules/app.js
@@ -1,26 +1,26 @@
+
+
 /**
 * Documento principal AngularJS para la aplicación WEB
 */
 'use strict';
+var app = angular.module('tripGripWebApp', ['ngRoute','angularModalService']);
 
-var app = angular.module('tripGripWebApp', ['ngRoute', 'angularModalService']);
+
 
 app.controller('Centro_AyudaController', ['$scope', 'ModalService', function($scope, ModalService) {
-
-  $scope.yesNoResult = null;
-/* 
-  $scope.showYesNo = function() {
-
+  $scope.showYesNo = function()
+  {
+   
     ModalService.showModal({
-      templateUrl: "yesno/yesno.html",
+      templateUrl: "partials/yesno.html",
       controller: "YesNoController"
     }).then(function(modal) {
       modal.element.modal();
       modal.close.then(function(result) {
         $scope.yesNoResult = result ? "You said Yes" : "You said No";
       });
-    });
+    }); 
 
   };
-*/
 }]);
